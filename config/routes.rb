3145db5087ec7b80531b659e1c4f5ec79528e92f
Tz <alexandru_tz@yahoo.com>
2023-02-24 17:02:12 +0200

    Added home, about, contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  ## get 'pages/home' ## replaced with:
+  root 'pages#home'
+
+  get 'about' => 'pages#about'
+  get 'contactus' => 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
